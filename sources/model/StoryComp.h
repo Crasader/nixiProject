4dc52455385947b34dd06ba8d821b6f161315bd7
ilakkey <happylakkey@gmail.com>
2016-06-21 16:31:32 +0800

    lakkey 160621:1631

--- sources/model/StoryComp.h
+++ sources/model/StoryComp.h
@@ -21,11 +21,13 @@ public:
     ~StoryComp();
     CREATE_FUNC(StoryComp);
     bool init();
-    
     void init_with_json(Value json);
     
 public:
     CCDictionary* cmpleted_story();
+    bool has_init_story();                          // 是否初始化已完成的剧情信息
+    bool has_passed(const char* story_index);       // 剧情是否已通过
+    CCArray* story_achievments(const char* story_index); // 剧情已获得成就列表(CCString)
     
 private:
     CCDictionary*           _cmpleted_story;
