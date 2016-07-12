01c4ddaa05aeaec83d7b532746750442bda2aecd
setvenli8283525 <510106010@qq.com>
2016-07-12 21:22:13 +0800

    by yang 07122122

---
+++ sources/view/mainScene/DragLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  DragLayer.cpp
+//  tiegao
+//
+//  Created by stevenLi on 16/7/12.
+//
+//
+
+#include "DragLayer.hpp"
---
+++ sources/view/mainScene/DragLayer.h
@@ -0,0 +1,14 @@
+//
+//  DragLayer.hpp
+//  tiegao
+//
+//  Created by stevenLi on 16/7/12.
+//
+//
+
+#ifndef DragLayer_hpp
+#define DragLayer_hpp
+
+#include <stdio.h>
+
+#endif /* DragLayer_hpp */
