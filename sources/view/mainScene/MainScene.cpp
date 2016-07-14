40755f0b0fab55c299314d9c7264b72dd1e40313
ilakkey <happylakkey@gmail.com>
2016-07-14 14:32:56 +0800

    lakkey 160714:1432

--- sources/view/mainScene/MainScene.cpp
+++ sources/view/mainScene/MainScene.cpp
@@ -32,9 +32,6 @@
 
 // --------------- test ----------------
 
-#include "SpecialManager.h"
-
-#include "StorySettlementOfTheAnimationLayer.h"
 
 MainScene::MainScene(){
     
@@ -688,9 +685,8 @@ void MainScene::homeCallBack(CCObject *pSender){
 
 void MainScene::shouchongCallBack(CCObject* pSender){
     if (isOk) {
-        SPECIAL->showStar2At(getParent(), DISPLAY->center(), 1);
-    }
     
+    }
 }
 
 void MainScene::huodongCallBack(CCObject* pSender){
@@ -701,13 +697,8 @@ void MainScene::huodongCallBack(CCObject* pSender){
 }
 
 void MainScene::qiandaoCallBack(CCObject* pSender){
-//    SPECIAL->showPetal2At(this->getScene(), DISPLAY->center(), 1);
-//    StorySettlementOfTheAnimationLayer* layer = StorySettlementOfTheAnimationLayer::create_with_index(1);
-//    this->addChild(layer, 1000);
-//    CONFIG->getMissionDialog(1, 2);
     if (isOk) {
-
-        CONFIG->getMissionDialog(1, 2);
+        NET->ranking_list_300();
     }
 }
 
