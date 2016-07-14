05805762fcd07ad85475b31c76ca9f1125c7dabc
ilakkey <happylakkey@gmail.com>
2016-07-14 21:21:12 +0800

    lakkey 160714:2121

--- sources/view/building/BuildingView.cpp
+++ sources/view/building/BuildingView.cpp
@@ -15,7 +15,8 @@
 #include "Loading2.h"
 
 BuildingView::~BuildingView() {
-    CC_SAFE_RELEASE_NULL(_floors);
+    CCLOG("BuildingView::~BuildingView() ");
+    CC_SAFE_DELETE(_floors);
 }
 
 BuildingView* BuildingView::create(int phase) {
@@ -48,15 +49,6 @@ bool BuildingView::init(int phase) {
     mask->setPosition(DISPLAY->center());
     this->addChild(mask);
     
-    float viewHeight = MIN(DISPLAY->H() * 0.85, FLOOR_CELL_HEIGHT * (_phase + 3));
-    _tbView = UnreusedTableView::create(this, CCSizeMake(FLOOR_CELL_WIDTH, viewHeight));
-    _tbView->setDirection(kCCScrollViewDirectionVertical);
-    _tbView->setVerticalFillOrder(kCCTableViewFillBottomUp);
-    _tbView->setPosition(ccp((DISPLAY->W() - FLOOR_CELL_WIDTH) * 0.5, DISPLAY->H() * (1 - 0.85) * 0.5));
-    _tbView->setEageBounched(false);
-    _tbView->setDelegate(this);
-    this->addChild(_tbView);
-    
     CCSprite* txt_close = CCSprite::create("res/pic/txt_close.png");
     txt_close->setPosition(ccp(DISPLAY->halfW(), DISPLAY->H() * 0.06));
     this->addChild(txt_close);
@@ -67,6 +59,16 @@ bool BuildingView::init(int phase) {
 void BuildingView::onEnter() {
     CCLayer::onEnter();
     
+    float viewHeight = MIN(DISPLAY->H() * 0.85, FLOOR_CELL_HEIGHT * (_phase + 3));
+    _tbView = UnreusedTableView::create(this, CCSizeMake(FLOOR_CELL_WIDTH, viewHeight));
+    _tbView->setDirection(kCCScrollViewDirectionVertical);
+    _tbView->setVerticalFillOrder(kCCTableViewFillBottomUp);
+    _tbView->setPosition(ccp((DISPLAY->W() - FLOOR_CELL_WIDTH) * 0.5, DISPLAY->H() * (1 - 0.85) * 0.5));
+    _tbView->setEageBounched(false);
+    _tbView->setDelegate(this);
+    this->addChild(_tbView);
+    
+    //
     CCNotificationCenter* nc = CCNotificationCenter::sharedNotificationCenter();
     nc->addObserver(this, SEL_CallFuncO(&BuildingView::nc_collect_coin), "COLLECT_COIN", NULL);
     nc->addObserver(this, SEL_CallFuncO(&BuildingView::nc_take_income), "TAKE_INCOME", NULL);
@@ -143,11 +145,14 @@ void BuildingView::nc_collect_coin_201(CCObject *pObj) {
 
 void BuildingView::nc_take_income_203(CCObject *pObj) {
     LOADING->remove();
+    CCNotificationCenter::sharedNotificationCenter()->postNotification("UpdataMoney");
+}
+
+void BuildingView::update_coffers() {
     if (_tbView) {
         FloorCell* floor = (FloorCell*)_tbView->cellAtIndex(0);
         floor->update_coffers();
     }
-    CCNotificationCenter::sharedNotificationCenter()->postNotification("UpdataMoney");
 }
 
 #pragma mark - CCTableViewDataSource
