d3e262f6cf55a4b9f5ba4f2cc4e2752a0979b4e6
ilakkey <happylakkey@gmail.com>
2016-08-11 20:10:02 +0800

    temp

--- sources/manager/DataManager.cpp
+++ sources/manager/DataManager.cpp
@@ -395,6 +395,7 @@ void DataManager::handle_protocol(int cid, Value content) {
             _player->init_with_json(content["player"]);
             this->creat_Energy_Time();
             _operation->replace_gashapon_user(content["gashapon"]);
+            pData = AppUtil::dictionary_with_json(content["result"]);
         } break;
             
         case 200: {
